ccdcfd42598ae267106e342dced5b9212770ffd3
Shinya Inomata <shinya.inomata@gmail.com>
2020-09-19 19:50:19 +0900

    Initial Commit

---
+++ TechMon200919/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon200919
+//
+//  Created by Shinya  on 2020/09/19.
+//  Copyright © 2020 Shinya . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
